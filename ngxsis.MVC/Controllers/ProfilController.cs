d723756d4a67bf4482e15acc8b5dbc1fe1cc0596
rizalasihombing <surelrizal@gmail.com>
2019-05-31 16:17:08 +0700

    tambah profil dan unduh

---
+++ ngxsis.MVC/Controllers/ProfilController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ngxsis.Repository;
+using ngxsis.ViewModel;
+
+namespace ngxsis.MVC.Controllers
+{
+    public class ProfilController : Controller
+    {
+        // GET: Profil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return PartialView("_List", ProfilRepo.All());
+        }
+    }
+}
