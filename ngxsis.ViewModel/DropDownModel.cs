3367a14dd36f22ad4611f436b1e974e48e3bcfac
fatkhurrozi <fatkhurrosii@gmail.com>
2019-06-03 07:14:50 +0700

    Menambahkan Pop Up tambah pelamar ke menu pelamar

---
+++ ngxsis.ViewModel/DropDownModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngxsis.ViewModel
+{
+   public class DropDownModel
+    {
+        public long id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { get; set; }
+
+        [StringLength(100)]
+        public string description { get; set; }
+
+
+
+    }
+}
---
+++ ngxsis.ViewModel/ValidationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngxsis.ViewModel
+{
+  public class ValidationModel
+    {
+        public string email { get; set; }
+
+        public string phone_number1 { get; set; }
+
+        public long identity_type_id { get; set; }
+        public string identity_no { get; set; }
+        public string identity_name { get; set; }
+
+    }
+}
